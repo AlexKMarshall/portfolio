3ca9e3330969cf7dabc53267897d0c05eee6229b
Alex <alexmarshall@qogita.com>
2026-02-22 17:17:30 +0000

    add code block example (#3)

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,10 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
 
-declare module 'virtual:image-assets' {
-	const imageAssets: Record<
-		string,
-		{ url: string; alt: string; caption?: string; attribution?: string }
-	>;
-	export { imageAssets };
-	export default imageAssets;
+declare module "virtual:image-assets" {
+  const imageAssets: Record<
+    string,
+    { url: string; alt: string; caption?: string; attribution?: string }
+  >;
+  export { imageAssets };
+  export default imageAssets;
 }
