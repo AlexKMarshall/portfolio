a5a66ca4539bc590d4be98ffa42924e07982043a
AlexKMarshall <alexmarshall@qogita.com>
2026-02-22 17:10:18 +0000

    add code block example

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import tailwindcss from '@tailwindcss/vite';
-import imageAssetsPlugin from './vite-plugin-image-assets.mjs';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import tailwindcss from "@tailwindcss/vite";
+import imageAssetsPlugin from "./vite-plugin-image-assets.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [mdx()],
-	vite: {
-		plugins: [imageAssetsPlugin(), tailwindcss()],
-	},
+  integrations: [mdx()],
+  markdown: {
+    syntaxHighlight: false,
+  },
+  vite: {
+    plugins: [imageAssetsPlugin(), tailwindcss()],
+  },
 });
